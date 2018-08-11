eadf7c2a458098cdd35a146ae4ec8d5cd7557eea
tigerfarm <tigerfarm@gmail.com>
2018-08-11 14:59:54 -0700

    updates

--- README.md
+++ README.md
@@ -1,59 +1,39 @@
 # Owl Group SMS Application
 
-Requirements:
+This is the documented steps to configure the Group SMS application using the Twilio Console.
+After, is documentation for developers.
+
+## Requirements:
 
 - Twilio account. A free Trial account will work.
-- Node.JS installed to run programs locally on your computer. Note, the Twilio Node.JS Helper Library is required.
-
-Note, it is possible to create a Service Map with an empty name (UniqueName=""). However, you cannot delete it with delete Map program (delMap.php).
-
-## Files
-
-Programs to manage Sync service Map items:
-- [1initDeleteMap.js](1initDeleteMap.js) : Delete a specific Sync Map Item, which deletes all the related Items.
-- [2initCreateMap.js](2initCreateMap.js) : Given a Sync service, create a Map.
-- [4retrieveMaps.js](4retrieveMaps.js) : List Sync Maps.
-- [createMapItem.js](createMapItem.js) : Given a Sync service and a Map, create a Map Item.
-- [updateMapItem.js](listMapItem.js) : Update a specific Sync Map Item.
-- [setvars.sh](setvars.sh) : Set program environment variables.
-
-After creating your Sync Service, create environment variables for use in these repository's programs:
-````
-ACCOUNT_SID=your_account_SID
-AUTH_TOKEN=your_account_auth_token
-SYNC_SERVICE_SID=your_sync_service_SID
-SYNC_MAP_NAME=counters
-export ACCOUNT_SID
-export AUTH_TOKEN
-export SYNC_SERVICE_SID
-export SYNC_MAP_NAME
-````
-Note, you can use the shell script to maintain your variables ([setvars.sh](setvars.sh)).
-````
-Run using: source setvars.sh
-````
+- For developers, Node.JS installed to run the Group SMS development programs locally on your computer.
+Note, the Twilio Node.JS Helper Library is required.
 
 ## Configure Group SMS
-
-Open a Twilio account to manage your Twilio resources:
 
 ````
 Open a Twilio account to manage your Twilio resources:
 https://www.twilio.com
 For Group SMS, you are required you to upgrade your account.
+
 Search and buy an SMS capable phone number:
 https://www.twilio.com/console/phone-numbers/incoming
 In these steps, I use +12223331234 as the example phone number.
 The Twilio Phone Number is for members to send commands and broadcast messages.
-Twilio Functions runs the Group SMS application. Create a Function:
+
+Twilio Functions runs the Group SMS application.
+Create a Function:
 https://www.twilio.com/console/runtime/functions/manage
 Click the Create a new Function icon. In the New Function popup, click Blank and click Create. Set:
 Function name: Group SMS
 Set the /path to /groupsms.
 Copy and paste the code from this link: groupSms.js, into the Code text area.
 Click Save.
+````
+Function screen print:
 
-
+<img src="ScreenPrintFunction.jpg"/>
+````
 Create a Messaging Service(Twilio Copilot).
 In a new tab (keep the Function tab open), go to:
 https://www.twilio.com/console/sms/services 
@@ -71,6 +51,10 @@ Click Add Selected. Your number is now listed.
 ````
 Note, the Twilio Copilot Messaging Service has attributes you can set to control the sending of messages. For Group SMS, use the defaults.
 
+Messaging Service screen print:
+
+<img src="ScreenPrintMS.jpg"/>
+
 ````
 Create a Notify Service to broadcast group messages:
 https://www.twilio.com/console/notify/services
@@ -82,6 +66,9 @@ Click Save.
 ````
 Keep this tab open because the Notify service SID is used when configuring the Group SMS Twilio Function.
 
+Notify Service screen print:
+
+<img src="ScreenPrintNS.jpg"/>
 ````
 Create a Sync Service to manage the member data:
 https://www.twilio.com/console/sync/services
@@ -91,6 +78,9 @@ Click Create.
 ````
 Keep this tab open because the Sync service SID is used when configuring the Group SMS Twilio Function.
 
+Sync Service screen print:
+
+<img src="ScreenPrintSS.jpg"/>
 ````
 Configure the Function to use the Sync and Notify services:
 https://www.twilio.com/console/runtime/functions/configure
@@ -101,3 +91,46 @@ Key: NOTIFY_SERVICE_SID and value: your_notify_service_SID
 Key: SYNC_SERVICE_SID and value: your_sync_service_SID
 Click Save.
 ````
+
+Sync Service screen print:
+
+<img src="ScreenPrintSS.jpg"/>
+
+## Documentation for Developers
+
+### Files
+
+Programs to manage Sync service Map items:
+- [1initDeleteMap.js](1initDeleteMap.js) : Delete a specific Sync Map Item, which deletes all the related Items.
+- [2initCreateMap.js](2initCreateMap.js) : Given a Sync service, create a Map.
+- [3initSmsSend.js](3initSmsSend.js)     : Send the initialization SMS message, "!init Harry".
+- [4retrieveMaps.js](4retrieveMaps.js)   : List Sync Maps.
+- [5smsList.js](5smsList.js)             : List received SMS message from the group phone number.
+- [6smsSend.js](6smsSend.js)             : Send an SMS message to the group phone number.
+- [groupSms.js](groupSms.js)             : Group SMS development program.
+- [groupSmsFunction.js](groupSmsFunction.js) : Group SMS Twilio Function code based on groupSms.js.
+- [createMapItem.js](createMapItem.js)   : Given a Sync service and a Map, create a Map Item.
+- [retrieveMapItemsEach.js](retrieveMapItemsEach.js)       : Retrieve Sync service Map Items.
+- [retrieveMapItemsForEach.js](retrieveMapItemsForEach.js) : Retrieve Sync service Map Items, method used in groupSms.js.
+- [updateMapItemAuthorize.js](updateMapItemAuthorize.js)   : Update a specific Sync Map Item authorization value.
+- [updateMapItem.js](updateMapItem.js)   : Update a specific Sync Map Item data.
+- [echoVars.js](echoVars.js)             : Echo environment variables that are used in these Node.JS programs.
+- [setvars.sh](setvars.sh)               : Set program environment variables.
+
+After creating your Sync Service, create environment variables for use in these repository's programs:
+````
+ACCOUNT_SID=your_account_SID
+AUTH_TOKEN=your_account_auth_token
+SYNC_SERVICE_SID=your_sync_service_SID
+SYNC_MAP_NAME=counters
+export ACCOUNT_SID
+export AUTH_TOKEN
+export SYNC_SERVICE_SID
+export SYNC_MAP_NAME
+````
+Note, you can use the shell script to maintain your variables ([setvars.sh](setvars.sh)).
+````
+Run using: source setvars.sh
+````
+Cheers,
+Stacy David
---
+++ groupSmsFunction.js
@@ -0,0 +1,399 @@
+// Group SMS function
+//
+'use strict';
+//
+const authorizedDefault = "self";   // Default, "self", does not require new subscribers to be authorized.
+//
+const syncServiceSid = process.env.SYNC_SERVICE_SID;
+const notifyServiceSid = process.env.NOTIFY_SERVICE_SID;
+//
+const sync = Runtime.getSync({serviceName: syncServiceSid});
+const client = Twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+const notify = client.notify.services(notifyServiceSid);
+//
+const initSuccessMessage = '+ Group phone number initialized and you are subscribed as the admin.';
+const initFailMessageNameRequired = '- Init name required: "!init name".';
+const initFailMessage = '- Group phone number already initialized.';
+const helpMessage = 'Help: Text "!subscribe name" to join. "!authorize +PhoneNumber" to accept a new subscriber. "!unsubscribe" to leave the group. "!who" to receive a group list.';
+const subscribeSuccessMessage = "+ You are subscribed to this Group's SMS messages.";
+const subscribeFailMessage = '- Subscription process failed, try again.';
+const subscribeFailMessageNameRequired = '- Subscription name required: "!subscribe name".';
+const authorizeSuccessMessage = '+ You have authorized: ';
+const authorizeFailMessage = '- Failed to authorize.';
+const authorizeFailMessageNotAuthorized = '- You are not authorized to authorize.';
+const authorizeFailMessageAlreadyAuthorized = '- Already authorized.';
+const authorizeFailMessageNumberRequired = '- Authorize phone number required: "!authorize phone-number".';
+const UnsubscribeMessage = '+ You have been unsubscribed from this group phone number.';
+const UnsubscribeFailMessage = '- Failed to unsubscribe.';
+const whoMessage = "+ Members: ";
+const whoSuccessMessage = '';
+const whoFailMessage = '- You must be a member of the group to make this request.';
+const whoFailMessageNotAuthorized = '- You are not authorized.';
+const broadcastSuccessMessage = '+ Your message was broadcast to the group.';
+const broadcastFailMessage = '- Your message failed to send, try again.';
+const broadcastFailMessageNotAuthorized = '- You are not authorized to broadcast messages.';
+const broadcastNotAuthorizedMessage = '- You are not part of the group.';
+// -----------------------------------------------------------------------------
+class Command {
+    // Create an instance with arguments from the incoming SMS
+    constructor(event) {
+        this.toNumber = event.To.trim();
+        this.fromNumber = event.From.trim();
+        this.body = event.Body.trim() || '';
+        this.event = event;
+        if (this.fromNumber.indexOf('+') !== 0) {
+            // If missing "+country code", fix it
+            this.fromNumber = `+${this.fromNumber}`;
+        }
+        if (this.toNumber.indexOf('+') !== 0) {
+            // If missing "+country code", fix it
+            this.toNumber = `+${this.toNumber}`;
+        }
+        // console.log("+ this.fromNumber: " + this.fromNumber);
+        this.smsTextArray = this.body.split(' ');
+        this.word1 = this.body.trim().split(' ')[0].toLowerCase();
+        this.word2 = '';
+        if (this.word1 === "!") {
+            this.word1 = "!" + this.smsTextArray[1].trim().toLowerCase();
+            if (this.smsTextArray.length === 3) {
+                this.word2 = this.smsTextArray[2].trim();
+            }
+        } else {
+            if (this.smsTextArray.length === 2) {
+                this.word2 = this.smsTextArray[1].trim();
+            }
+        }
+
+    }
+    // Get an array of arguments after the first word for a command
+    get commandArguments() {
+        return this.body.trim().split(' ').slice(1);
+    }
+    // Get the full text after the command with spaces reinserted
+    get commandText() {
+        return this.commandArguments.join(' ');
+    }
+    // Execute command async (to be overridden by subclasses)
+    run(callback) {
+        callback(null, 'Command not implemented.');
+    }
+}
+
+class HelpCommand extends Command {
+    run(callback) {
+        // console.log("++ callback: " + helpMessage);
+        callback(null, helpMessage);
+                    
+        // ----------------
+        // let whoInstance = new WhoCommand({Body: "who", From: process.env.PHONE_NUMBER_3, To: process.env.PHONE_NUMBER_1});
+        // whoInstance.run((err, message) => {
+        //    let twiml2 = new Twilio.twiml.MessagingResponse();
+        //    if (err) {
+        //        message = 'There was a problem with your request.';
+        //    }
+        //    twiml2.message(message);
+        //    callback(null, twiml2);
+        // });
+        // ----------------
+    }
+}
+
+class InitCommand extends Command {
+    run(callback) {
+        if (this.word2 === "") {
+            callback(null, initFailMessageNameRequired);
+            return;
+        }
+        // The Twilio to-phone-number, the group phone number. This is the Sync Map name.
+        sync.syncMaps.create({ttl: 0, uniqueName: this.toNumber})
+        .then((sync_map) => {
+            console.log("+ Initialized, created group SMS phone number Map: " + sync_map.uniqueName);
+            //
+            // Create the Admin's Sync enter using their phone number as the key and broadcast a message to let them know.
+            let theData = {'name': this.word2, 'authorized': 'admin'};
+            sync.syncMaps(this.toNumber).syncMapItems
+                .create({key: this.fromNumber, data: theData})
+                .then((sync_map_item) => {
+                    console.log("+ Group initialized and you are subscribed: " + this.word2 + " " + this.fromNumber);
+                    callback(null, initSuccessMessage);
+                }).catch(function (error) {
+                callback(error, subscribeFailMessage);
+            });
+        })
+        .catch(function (error) {
+            callback(error, initFailMessage);
+        });
+    }
+}
+
+class AuthorizeCommand extends Command {
+    run(callback) {
+        if (this.word2 === "") {
+            callback(null, authorizeFailMessageNumberRequired);
+            return;
+        }
+
+        sync.syncMaps(this.toNumber).syncMapItems(this.fromNumber).fetch()
+        .then((syncMapItems) => {
+            let senderName = syncMapItems.data.name;
+            let authorized = syncMapItems.data.authorized;
+            console.log("+ Sender name: " + senderName + ", authorized: " + authorized);
+            if (authorized === 'new') {
+                callback(null, authorizeFailMessageNotAuthorized);
+                return;
+            }
+            sync.syncMaps(this.toNumber).syncMapItems(this.word2).fetch()
+            .then((syncMapItems) => {
+                let personName = syncMapItems.data.name;
+                let authorized = syncMapItems.data.authorized;
+                console.log("+ name: " + personName + ", authorized: " + syncMapItems.data.authorized);
+                if (authorized !== 'new') {
+                    callback(null, authorizeFailMessageAlreadyAuthorized);
+                    return;
+                }
+                let theData = {'name': personName, 'authorized': this.fromNumber};
+                sync.syncMaps(this.toNumber).syncMapItems(this.word2)
+                    .update({key: this.word2, data: theData})
+                    .then((sync_map_item) => {
+                        console.log("+ Updated authorized, to: " + this.fromNumber);
+                        callback(null, authorizeSuccessMessage + personName);
+                    }).catch(function (error) {
+                        callback(error, authorizeFailMessage);
+                    });
+            }).catch(function (error) {
+                console.log("- AuthorizeCommand, retrieve parameter:  " + error);
+                callback(error, authorizeFailMessage);
+            });
+        }).catch(function (error) {
+            console.log("- AuthorizeCommand, retrieve from-phone-number:  " + error);
+                callback(error, authorizeFailMessage);
+        });
+    } // run(callback)
+}
+
+class SubscribeCommand extends Command {
+    // Add the person into the DB.
+    // Broadcast that they have joined.
+    // Need error checking for this.word2, that it is valid.
+    run(callback) {
+        if (this.word2 === "") {
+            callback(null, subscribeFailMessageNameRequired);
+            return;
+        }
+        let theData = {'name': this.word2, 'authorized': authorizedDefault};
+        sync.syncMaps(this.toNumber).syncMapItems
+        .create({key: this.fromNumber, data: theData})
+        .then((sync_map_item) => {
+            console.log("+ Subscribed, name: " + this.word2 + " " + this.fromNumber);
+            // ---------------------------------------------------------------------
+            // Broadcast notice of new subscriber.
+            let counter = 0;
+            let sendList = [];
+            let sendNameList = [];
+            sync.syncMaps(this.toNumber).syncMapItems.list()
+            .then(syncMapItems => {
+                syncMapItems.forEach((syncMapItem) => {
+                    // console.log("+ Key: " + syncMapItem.key + ", authorized: " + syncMapItem.data.authorized);
+                    if (this.fromNumber !== syncMapItem.key && syncMapItem.data.authorized !== "new") {
+                        // Don't send to the sender, nor to unauthorized numbers ("new").
+                        sendList[counter] = JSON.stringify({"binding_type": "sms", "address": syncMapItem.key});
+                        sendNameList[counter] = syncMapItem.data.name;
+                        counter += 1;
+                    }
+                });
+                if (counter === 0) {
+                    console.log("+ New subscription notice not sent because there is no one yet to receive the notice.");
+                    callback(null, subscribeSuccessMessage);
+                    return;
+                }
+                let theMessage = "Application notice, new ";
+                if (authorizedDefault === "new") {
+                    theMessage += "unauthorized ";
+                }
+                theMessage += "group subscriber: " + this.word2;
+                console.log("+ The message |" + theMessage + "| " + " Sent to: " + sendNameList);
+                notify.notifications.create({body: theMessage, toBinding: sendList})
+                .then((response) => {
+                    console.log("+ Notify response.sid: " + response.sid);
+                    callback(null, subscribeSuccessMessage + ' Notice of your new subscription was sent to the group.');
+                }).catch(err => {
+                    // console.log(err);
+                    callback(err, broadcastFailMessage);
+                });
+            });
+            // ---------------------------------------------------------------------
+        }).catch(function (error) {
+            callback(error, subscribeFailMessage);
+        });
+    }
+}
+
+class UnsubscribeCommand extends Command {
+    // Remove the person into the DB.
+    run(callback) {
+        sync.syncMaps(this.toNumber).syncMapItems(this.fromNumber)
+            .remove()
+            .then((sync_map) => {
+                console.log("+ Deleted.");
+                callback(null, UnsubscribeMessage);
+            }).catch(function (error) {
+            console.log("- " + error);
+            callback(error, UnsubscribeFailMessage);
+        }); 
+    }
+}
+
+class WhoCommand extends Command {
+    run(callback) {
+        let returnMessage = '';
+        // Check that the requester is in the group.
+        // Need a proper error message returned to the requester.
+        sync.syncMaps(this.toNumber).syncMapItems(this.fromNumber).fetch()
+        .then((syncMapItems) => {
+            let senderName = syncMapItems.data.name;
+            let authorized = syncMapItems.data.authorized;
+            console.log("+ Sender name: " + senderName + ", authorized: " + authorized);
+            if (authorized === 'new') {
+                callback(null, whoFailMessageNotAuthorized);
+                return;
+            }
+            sync.syncMaps(this.toNumber).syncMapItems.list()
+            .then(
+                syncMapItems => {
+                    syncMapItems.forEach((syncMapItem) => {
+                        authorized = syncMapItem.data.authorized;
+                        // console.log("+ Key: " + syncMapItem.key + ", authorized: " + authorized);
+                        if (returnMessage === '') {
+                            returnMessage = syncMapItem.data.name;
+                        } else {
+                            returnMessage += ", " + syncMapItem.data.name;
+                        }
+                        if (authorized === 'new') {
+                            returnMessage += '(new)';
+                        }
+                    });
+                    callback(null, whoMessage + returnMessage);
+                });
+            }).catch(function (error) {
+                callback(error, whoFailMessage);
+            });
+    }
+}
+
+class BroadcastTheMessage extends Command {
+    run(callback) {
+    
+        sync.syncMaps(this.toNumber).syncMapItems(this.fromNumber).fetch()
+        .then((syncMapItems) => {
+            // Check that the requester is in the group and that they are not "new".
+            let senderName = syncMapItems.data.name;
+            let authorized = syncMapItems.data.authorized;
+            console.log("+ Sender name: " + senderName + ", authorized: " + authorized);
+            if (authorized === 'new') {
+                callback(null, broadcastFailMessageNotAuthorized);
+                return;
+            }
+            // ---------------------------------------------------------------------
+            // Broadcast message.
+            let counter = 0;
+            let sendList = [];
+            let sendNameList = [];
+            sync.syncMaps(this.toNumber).syncMapItems.list()
+            .then(syncMapItems => {
+                syncMapItems.forEach((syncMapItem) => {
+                    // console.log("+ Key: " + syncMapItem.key + ", authorized: " + syncMapItem.data.authorized);
+                    if (this.fromNumber !== syncMapItem.key && syncMapItem.data.authorized !== "new") {
+                        // Don't send to the sender, nor to unauthorized numbers ("new").
+                        sendList[counter] = JSON.stringify({"binding_type": "sms", "address": syncMapItem.key});
+                        sendNameList[counter] = syncMapItem.data.name;
+                        counter += 1;
+                    }
+                });
+                if (counter === 0) {
+                    console.log("+ New subscription notice not sent because there is no one yet to receive the notice.");
+                    return;
+                }
+                let theMessage = "From: " + senderName + ", " + this.body;
+                console.log("+ The message |" + theMessage + "| " + " Sent to: " + sendNameList);
+                notify.notifications.create({body: theMessage, toBinding: sendList})
+                .then((response) => {
+                    console.log("+ Notify response.sid: " + response.sid);
+                    // callback(null, broadcastSuccessMessage);
+                    callback(null, null);
+                }).catch(err => {
+                    // console.log(err);
+                    callback(err, broadcastFailMessage);
+                });
+            });
+            // ---------------------------------------------------------------------
+        }).catch(function (error) {
+            callback(error, broadcastFailMessage);
+        });
+    }
+}
+
+// Handle incoming SMS commands
+//
+// For testing: https://obedient-machine-3163.twil.io/groupsms?To=+16503791233&From=6508661111&Body=okay
+//
+exports.handler = (context, event, callback) => {
+    //
+    let twiml = new Twilio.twiml.MessagingResponse();
+    //
+    let smsText = event.Body || '';
+    let smsTextArray = smsText.split(' ');
+    let cmd = smsText.trim().split(' ')[0].toLowerCase();
+    if (cmd === "!") {
+        cmd = "!" + smsTextArray[1].trim().toLowerCase();
+    }
+    let echoSms = "+ Text |" + smsText + "| cmd: " + cmd + ", From: " + event.From + ", To: " + event.To;
+    console.log(echoSms);
+    let cmdInstance;
+    switch (cmd) {
+        case '!subscribe':
+            cmdInstance = new SubscribeCommand(event);
+            break;
+        case '!authorize':
+            cmdInstance = new AuthorizeCommand(event);
+            break;
+        case '!unsubscribe':
+            cmdInstance = new UnsubscribeCommand(event);
+            break;
+        case '!who':
+            cmdInstance = new WhoCommand(event);
+            break;
+        case '!help':
+            cmdInstance = new HelpCommand(event);
+            break;
+        case '!init':
+            cmdInstance = new InitCommand(event);
+            break;
+        default:
+            cmdInstance = new BroadcastTheMessage(event);
+    }
+    cmdInstance.run((err, message) => {
+        if (err) {
+            // console.log(err);
+            console.log("- cmdInstance.run, " + cmdInstance.word1 + " error: " + err.status + ":" + err.message);
+            if (err.status === 409 && cmdInstance.word1 === '!subscribe') {
+                message = '- You are already subscribed.';
+            } else if (err.status === 404 && cmdInstance.word1 === '!unsubscribe') {
+                message = '- You are not subscribed.';
+            } else if (err.status === 404) {
+                message = 'There was a problem with your request, value not found: ' + cmdInstance.word2;
+            } else if (err.status === 409 && cmdInstance.word1 === '!init') {
+                message = initFailMessage;
+            } else {
+                message = 'There was a problem with your request. Please try again.';
+            }
+        }
+        if (message === null) {
+            console.log("+ No reply.");
+            callback(null);
+        } else {
+            console.log("+ Reply message: " + message);
+            twiml.message(message);
+            callback(null, twiml);
+        }
+    });
+}
